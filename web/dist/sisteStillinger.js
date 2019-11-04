26c32b7a8203f627e7cadde2a5645804f3952d5a
Tov Are Jacobsen <tovare@Tovs-MacBook-Pro.local>
2019-11-04 05:03:42 +0100

    public

--- web/dist/sisteStillinger.js
+++ web/dist/sisteStillinger.js
@@ -2257,7 +2257,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "59804" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "60858" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
