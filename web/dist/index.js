7702c9360fcf605c9df7d5e575d0469c396c7440
Tov Are Jacobsen <tovare@Tovs-MacBook-Pro.local>
2019-11-05 00:37:12 +0100

    overskrift

--- web/dist/index.js
+++ web/dist/index.js
@@ -212,7 +212,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "60293" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "64020" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
